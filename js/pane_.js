447eeabd9b97519166be41a7c86d7689e341d7a4
tawanabean <91478129+tawanabean@users.noreply.github.com>
2024-04-29 07:14:09 -0400

    Add files via upload

---
+++ js/pane_.js
@@ -0,0 +1,53 @@
+/**
+ * @author Tawana
+ * 
+ * Copyright 2011, Tawana Bean
+ *
+ * @date : Jun 22, 2021 
+ */
+
+"use strict";
+
+var PanelsItem = function(objhere) {
+
+
+}
+
+PanelsItem.prototype.onclick = function(ref, panels) {
+
+}
+
+
+var Panels = function(objhere) {
+
+
+
+}
+
+Panels.prototype.setallpanels = function(ref) {
+
+}
+
+Panels.prototype.setpanel = function(ref) {
+
+}
+
+Panels.prototype.getallpanels = function(ref) {
+
+}
+
+Panels.prototype.getpanel = function(ref) {
+
+}
+
+Panels.prototype.onclick = function(ref) {
+
+}
+
+Panels.prototype.show = function(ref) {
+
+}
+
+Panels.prototype.hide = function(ref) {
+
+}
